607e51ca2cbb3983b14b3dbca30a3efc6a16c242
Rasberry <rasberry@hotmail.com>
2013-09-22 18:24:56 -0600

    =Added custom palette =Added -m in memory mode for smaller maps

--- Program.cs
+++ Program.cs
@@ -21,27 +21,41 @@ namespace mcmappy
 				return;
 			}
 			
-			_mainThread = Thread.CurrentThread;
-			var timer = new System.Threading.Timer(Sample,null,TimeSpan.Zero,new TimeSpan(0,0,0,0,500));
+			//_mainThread = Thread.CurrentThread;
+			//var timer = new System.Threading.Timer(Sample,null,TimeSpan.Zero,new TimeSpan(0,0,0,0,500));
 			
-			string dest = args[0];
-			string dim = null;
-			if (args.Length > 1) { dim = args[1]; }
+			string dest=null,dim=null,pal=null;
+			bool inmem = false;
+			for(int a=0; a<args.Length; a++) {
+				string c = args[a];
+				if (c == "-d") { dim = args[++a]; }
+				else if (c == "-p") { pal = args[++a]; }
+				else if (c == "-m") { inmem = true; }
+				else { dest = args[a]; }
+			}
 			
+			if (String.IsNullOrEmpty(pal)) {
+				Console.WriteLine("no palette file specified. use -p (palette file)");
+				return;
+			}
+			LoadPalette(pal);
 			NbtWorld world = NbtWorld.Open(dest);
 			if (world == null) {
-				Console.WriteLine("world not loaded"); return;
+				Console.WriteLine("world not loaded");
+				return;
 			}
+			
 			IChunkManager cm = dim == null ? world.GetChunkManager() : world.GetChunkManager(dim);
 			string name = Path.GetFileName(dest);
 			
 			int mxdim=0,mydim=0,mzdim=0;
 			int mx=0,mz=0,sx=0,sz=0;
-			HashSet<int> idset = new HashSet<int>();
+			//HashSet<int> idset = new HashSet<int>();
+			
+			IEnumerable<ChunkRef> chunkList = inmem ? (IEnumerable<ChunkRef>)cm.ToList() : (IEnumerable<ChunkRef>)cm;
 			
 			Console.WriteLine("Calculating size...");
-			//var air = new List2D<int>();
-			foreach (ChunkRef chunk in cm) {
+			foreach (ChunkRef chunk in chunkList) {
 				int xdim = chunk.Blocks.XDim;
 				int ydim = chunk.Blocks.YDim;
 				int zdim = chunk.Blocks.ZDim;
@@ -53,17 +67,17 @@ namespace mcmappy
 				if (chunk.X < sx) { sx = chunk.X; }
 				if (chunk.Z < sz) { sz = chunk.Z; }
 				// x, z, y is the most efficient order to scan blocks
-				for(int x=0; x<xdim; x++) {
-					for(int z=0;z<zdim; z++) {
-						for(int y=0;y<ydim;y++) {
-							int id = chunk.Blocks.GetID(x,y,z);
-							idset.Add(id);
-							//if (id != 0 && air[x,z] < y) { air[x,z] = y; } //keep track of highest non-air block
-						}
-					}
-				}
+//				for(int x=0; x<xdim; x++) {
+//					for(int z=0;z<zdim; z++) {
+//						for(int y=0;y<ydim;y++) {
+//							int id = chunk.Blocks.GetID(x,y,z);
+//							int meta = chunk.Blocks.GetData(x,y,z);
+//							idset.Add(id);
+//						}
+//					}
+//				}
 			}
-			double idcount = idset.Count;
+			//double idcount = idset.Count;
 			int sizex = mxdim * (mx - sx + 1);
 			int sizez = mzdim * (mz - sz + 1);
 			Console.WriteLine("Creating "+mydim+" "+sizex+"x"+sizez+" images");
@@ -75,9 +89,8 @@ namespace mcmappy
 				LockBitmap lck = new LockBitmap(img);
 				lck.LockBits();
 			
-				foreach (ChunkRef chunk in cm) { //TODO this is the slow part
+				foreach (ChunkRef chunk in chunkList) { //TODO this is the slow part
 					if (y >= chunk.Blocks.YDim) { continue; } //make sure the current y exists
-					//if (air[chunk.X,chunk.Z] < y) { continue; } //skip if this chunk is just air
 				
 					int xdim = chunk.Blocks.XDim;
 					int zdim = chunk.Blocks.ZDim;
@@ -88,19 +101,21 @@ namespace mcmappy
 					for (x = 0; x < xdim; x++) {
 						for (z = 0; z < zdim; z++) {
 							int id = chunk.Blocks.GetID(x,y,z);
+							int meta = chunk.Blocks.GetData(x,y,z);
 							//BlockInfo info = chunk.Blocks.GetInfo(x, y, z);
-							lck.SetPixel(ox + x,oz + z,GetColorFromId(id,idcount));
+							lck.SetPixel(ox + x,oz + z,GetColorFromId(id,meta));
 							//Console.WriteLine("["+x+","+y+","+z+"] "+info.ID+" "+info.Name);
 						}
 					}
 				}
 				lck.UnlockBits();
-				img.Save(name+"_"+y+".png");
-				Console.WriteLine("Saved "+name+"_"+y+".png");
+				string outfile = name+"_"+y.ToString("000")+".png";
+				img.Save(outfile);
+				Console.WriteLine("Saved "+outfile);
 			}
 
 			_sameplingEnabled = false;
-			timer.Dispose(); //kill timer.
+			//timer.Dispose(); //kill timer.
 			foreach(var kvp in _speed.OrderByDescending(k => k.Value))
 			{
 				Console.WriteLine(kvp.Key+"\t"+kvp.Value);
@@ -137,7 +152,43 @@ namespace mcmappy
 		
 		private static Dictionary<int,Color> _colors = new Dictionary<int,Color>();
 		
-		private static Color GetColorFromId(int id,double idcount)
+		private static Color GetColorFromId(int id, int meta)
+		{
+			int key = meta !=0 ? -1*(meta + (id << 4)) : id;
+			Color c;
+			if (!_colors.TryGetValue(key, out c)) { //try id+meta
+				if (!_colors.TryGetValue(id, out c)) { //try just id
+					c = Color.Black;
+				}
+			}
+			return c;
+		}
+		
+		private static void LoadPalette(string file)
+		{
+			StreamReader sr = File.OpenText(file);
+			while(!sr.EndOfStream) {
+				string line = sr.ReadLine();
+				if (line.StartsWith(".")) {
+					string[] ops = line.Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries);
+					if (ops.Length < 8) { continue; }
+					int id,meta,r,g,b;
+					if (
+						int.TryParse(ops[1],out id)
+						&& int.TryParse(ops[4], out r)
+						&& int.TryParse(ops[5],out g)
+						&& int.TryParse(ops[6],out b)
+					) {
+						if (!int.TryParse(ops[2], out meta)) { meta = 0; }
+						int key = meta !=0 ? -1*(meta + (id << 4)) : id;
+						Color c = Color.FromArgb(r,g,b);
+						_colors[key] = c;
+					}
+				}
+			}
+		}
+		
+		private static Color GetColorFromId_old(int id,double idcount)
 		{
 			if (id == 0) { return Color.Black; }
 			Color c;
